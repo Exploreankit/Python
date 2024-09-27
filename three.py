682e3d442b244068a0ce21e71389900ee7660412
Exploreankit <ankitsingh7167@gmail.com>
2024-09-27 18:33:17 +0530

    third

---
+++ three.py
@@ -0,0 +1,10 @@
+#conditional statements
+light = input("light: ")
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("look")
+elif(light == "green"):
+    print("go")
+else:
+    print("light is broken")
