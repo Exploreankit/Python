af3f4163c2bce5e455b9a7e011d94101092e24c0
Exploreankit <ankitsingh7167@gmail.com>
2024-09-27 18:17:54 +0530

    second

---
+++ two.py
@@ -0,0 +1,15 @@
+name = "Ankit"
+age = 22
+price = 200.00
+
+print(name)
+print("My name is :", name)
+print(type(name))
+old = False # block F
+a = None
+print(type(old))
+print(type(a))
+name = input("name : ")
+age = int(input("age : "))
+print("My name is",name, "and I am",age,"years old")
+
